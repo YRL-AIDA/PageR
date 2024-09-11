0bcf221e134ab50f72edddabc1956ca3c84adb74
Dann38 <38dunn@gmail.com>
2024-09-11 12:32:34 +0800

    fix set image segment from many segments

--- src/pager/page_model/sub_models/phisical_model/phisical_model.py
+++ src/pager/page_model/sub_models/phisical_model/phisical_model.py
@@ -30,8 +30,7 @@ class WordsToOneBlock(BaseConverter):
     def convert(self, input_model: BaseSubModel, output_model: BaseSubModel)-> None:
         word_list: List[Word] = input_model.words
         segment = ImageSegment(0, 0, 1, 1)
-        segment.set_segment_2p(word_list[0].segment.get_segment_2p())
-        segment.set_segment_max_segments([word.segment for word in word_list[1:]])
+        segment.set_segment_max_segments([word.segment for word in word_list])
         block = Block(segment.get_segment_2p())
         block.set_words_from_dict([word.to_dict() for word in word_list])
         output_model.blocks.append(block)
