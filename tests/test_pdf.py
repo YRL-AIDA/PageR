fc4f329c660fbd68dd6ec3dc15f83b046a2dacbd
Dann38 <38dunn@gmail.com>
2024-09-20 16:58:40 +0800

    fix encoding

--- tests/test_pdf.py
+++ tests/test_pdf.py
@@ -8,7 +8,7 @@ class TestPDF(unittest.TestCase):
     pdf_model=PDFModel()
     pdf_model.read_from_file("files/text_header_table.pdf")    
 
-    with open("files/text_header_table_jar.json") as f:
+    with open("files/text_header_table_jar.json", 'r', encoding='utf-8') as f:
         true_dict = json.load(f) 
 
     def test_page_0_and_end(self) -> None:
@@ -24,7 +24,7 @@ class TestPDF(unittest.TestCase):
     def test_text(self) -> None:
         page0 = self.pdf_model.to_dict()
         text = "" 
-        with open('files/text_header_table_text.txt') as f:
+        with open('files/text_header_table_text.txt', 'r', encoding='utf-8') as f:
             for line in f:
                 text += " " + line[:-1] # delete \n
         text_ = ""
