b402d5fb321de4d092485e9b10fa0342750a70d3
daniil <Dann38>
2024-11-06 16:09:03 +0800

    test segmenter and classifier

--- tests/test_words_and_styles2phis_model.py
+++ tests/test_words_and_styles2phis_model.py
@@ -26,8 +26,15 @@ class TestWordsAndStyles2PhisModel(unittest.TestCase):
     name_class = ["no_struct", "text", "header", "list", "table"]
     def test_count_blocks(self) -> None:
         str_ = "/n".join([f"{i} - block: \t {b['text']}" for i, b in enumerate(self.phis['blocks'])])
-        self.assertGreaterEqual(len(self.phis['blocks']), 3, )
-    
+        self.assertGreaterEqual(len(self.phis['blocks']), 3, str_)
+
     def test_nead_class(self) -> None:
         for block in self.phis['blocks']:
             self.assertIn(block['label'], self.name_class, block['label'])
+
+    def test_header_and_text(self) -> None:
+        for block in self.phis['blocks']:
+            if "ПРИМЕР" in block['text'].split():
+                self.assertEqual(block['label'], 'header')
+            if "Второй" in block['text'].split() or "Третий" in block['text'].split():
+                self.assertEqual(block['label'], 'text')
