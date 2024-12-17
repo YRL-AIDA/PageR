7c6c28d8d5d2f4aaca5827e315c7566cd6878259
38dunn@gmail.com <Dann38>
2024-12-17 17:34:21 +0800

    test metrix

--- tests/test_words_and_styles2phis_model.py
+++ tests/test_words_and_styles2phis_model.py
@@ -4,7 +4,7 @@ from pager import (PageModel, PageModelUnit,
                    WordsAndStylesModel, PhisicalModel, 
                    WordsAndStylesToGNNBlocks)
 from pager.page_model.sub_models.dtype import ImageSegment
-from pager.metrics.uoi import segmenter_UoI as UoI
+from pager.metrics.uoi import segmenter_UoI as UoI, AP_and_AR_from_TP_FP_FN, TP_FP_FN_UoI
 
 class TestWordsAndStyles2PhisModel(unittest.TestCase):
     page = PageModel(page_units=[
@@ -29,7 +29,14 @@ class TestWordsAndStyles2PhisModel(unittest.TestCase):
     page.read_from_file('files/UoI.png')
     page.extract()
     phis2 = page.to_dict()
-
+    seg_pred = [ImageSegment(dict_2p=b)for b in phis2['blocks']]
+    seg_true = [ImageSegment(410, 50, 710, 110),
+                    ImageSegment(30, 150, 410, 300),
+                    ImageSegment(650, 150, 1060, 260),
+                    ImageSegment(30, 350, 1030, 430)]
+        
+    AP, AR = AP_and_AR_from_TP_FP_FN(*TP_FP_FN_UoI(seg_pred, seg_true))
+    UoI_rez = UoI(seg_pred, seg_true)
     name_class = ["no_struct", "text", "header", "list", "table"]
     def test_count_blocks(self) -> None:
         str_ = "/n".join([f"{i} - block: \t {b['text']}" for i, b in enumerate(self.phis['blocks'])])
@@ -46,11 +53,10 @@ class TestWordsAndStyles2PhisModel(unittest.TestCase):
             if "Второй" in block['text'].split() or "Третий" in block['text'].split():
                 self.assertEqual(block['label'], 'text')
     def test_uoi_classifier(self) -> None:
-        seg_pred = [ImageSegment(dict_2p=b)for b in self.phis2['blocks']]
-        seg_true = [ImageSegment(410, 50, 710, 110),
-                    ImageSegment(30, 150, 410, 300),
-                    ImageSegment(650, 150, 1060, 260),
-                    ImageSegment(30, 350, 1030, 430)]
+        self.assertGreaterEqual(self.UoI_rez, 0.5, f"UoI = {self.UoI_rez:.4f}")
         
-        UoI_rez = UoI(seg_pred, seg_true)
-        self.assertGreaterEqual(UoI_rez, 0.5, f"UoI = {UoI_rez:.4f}")
+    def test_AP(self) -> None:
+        self.assertGreaterEqual(self.AP, 0.5, f"AP = {self.AP:.6f}")
+
+    def test_AR(self) -> None:
+        self.assertGreaterEqual(self.AR, 0.5, f"AR = {self.AR:.6f}")
