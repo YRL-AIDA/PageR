08922eb0aa44d8f00a9af31e32cbfa74c82ffde1
Dann38 <38dunn@gmail.com>
2024-12-20 09:50:34 +0800

    test save

--- tests/test_ms_word_model.py
+++ tests/test_ms_word_model.py
@@ -6,11 +6,12 @@ from pager import MSWordModel
 from pager.metrics.levenshtein_distance import levenshtein_distance
 
 class TestMSWord(unittest.TestCase):
-    msword_model  = MSWordModel()
-    msword_model.read_from_file(os.path.join("files", "word.docx"))    
+    msword_model_output  = MSWordModel()
+    msword_model_input = MSWordModel()
+    msword_model_output.read_from_file(os.path.join("files", "word.docx"))    
 
     def test_read_text(self) -> None:
-        doc = self.msword_model.doc
+        doc = self.msword_model_output.doc
         all_paras = doc.paragraphs
         self.assertEqual(all_paras[0].text, "Заголовок 1", all_paras[0])
         self.assertEqual(all_paras[1].text, "Подзаголовок", all_paras[1])
@@ -19,7 +20,7 @@ class TestMSWord(unittest.TestCase):
         self.assertEqual(all_paras[4].text, "лист.", all_paras[4])
 
     def test_read_style(self) -> None:
-        doc = self.msword_model.doc
+        doc = self.msword_model_output.doc
         all_paras = doc.paragraphs
         header1 = all_paras[0].style
         header2 = all_paras[1].style
@@ -33,11 +34,20 @@ class TestMSWord(unittest.TestCase):
         self.assertEqual(list_1, list_2)
 
     def test_to_dict(self) -> None:
-        res = self.msword_model.to_dict()
+        res = self.msword_model_output.to_dict()
         self.assertIn("styles", res.keys(), res.keys())
         self.assertEqual(len(res["styles"]), 4, res.keys())
         self.assertIn("blocks", res.keys(), res.keys())
         self.assertEqual(len(res["blocks"]), 5, res.keys())
+
+    def test_from_dict(self) -> None:
+        self.msword_model_input.from_dict(self.msword_model_output.to_dict())
+        path = os.path.join("files", "tmp_word.docx")
+        if path in  os.listdir("files"):
+            os.remove(path)
+        self.msword_model_input.save_doc(path)
+        self.assertIn("tmp_word.docx", os.listdir("files"))
+
 
 
 
