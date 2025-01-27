7d08f6933fe0d76a8e57a61c4db036c02fb7b004
38dunn@gmail.com <Dann38>
2025-01-27 15:02:34 +0800

    test cnn_style_word_model and vec_word

---
+++ tests/test_words_and_styles2sp_graph.py
@@ -0,0 +1,60 @@
+import unittest
+from pager import (PageModel, PageModelUnit,
+                   ImageModel, ImageToWordsAndStyles,
+                   WordsAndStylesModel, WordsAndStylesToSpGraph4N,
+                          WordsAndStylesToSpDelaunayGraph, 
+                   SpGraph4NModel)
+from pager.page_model.sub_models.dtype import ImageSegment
+import numpy as np
+
+class TestWordsAndStyles2PhisModel(unittest.TestCase):
+    page_4n = PageModel(page_units=[
+        PageModelUnit(id="image_model", 
+                      sub_model=ImageModel(), 
+                      extractors=[], 
+                      converters={}),
+        PageModelUnit(id="words_and_styles_model", 
+                      sub_model=WordsAndStylesModel(), 
+                      extractors=[], 
+                      converters={"image_model": ImageToWordsAndStyles()}),
+        PageModelUnit(id="graph_model", 
+                      sub_model=SpGraph4NModel(), 
+                      extractors=[], 
+                      converters={"words_and_styles_model": WordsAndStylesToSpGraph4N(conf={"with_text": True})})
+        ])
+    page_delaunay= PageModel(page_units=[
+        PageModelUnit(id="image_model", 
+                      sub_model=ImageModel(), 
+                      extractors=[], 
+                      converters={}),
+        PageModelUnit(id="words_and_styles_model", 
+                      sub_model=WordsAndStylesModel(), 
+                      extractors=[], 
+                      converters={"image_model": ImageToWordsAndStyles()}),
+        PageModelUnit(id="graph_model", 
+                      sub_model=SpGraph4NModel(), 
+                      extractors=[], 
+                      converters={"words_and_styles_model": WordsAndStylesToSpDelaunayGraph(conf={"with_text": True})})
+        ])
+
+    page_4n.read_from_file('files/segment_test.png')
+    page_4n.extract()
+    g1 = page_4n.to_dict()    
+
+    page_delaunay.read_from_file('files/segment_test.png')
+    page_delaunay.extract()
+    g2 = page_delaunay.to_dict()
+    
+    def test_size_nodes_feature_4n(self) -> None:
+        N, M = np.array(self.g1['nodes_feature']).shape
+        self.assertEqual(N, 12, f"Count nodes = {N}")
+        # WORD=32 + STYLE=7 + POSITION=2
+        self.assertEqual(M, 32+7+2, f"Count nodes = {M}")
+
+    def test_size_nodes_feature_delaunay(self) -> None:
+        N, M = np.array(self.g2['nodes_feature']).shape
+        self.assertEqual(N, 12, f"Count nodes = {N}")
+        # WORD=32 + STYLE=7 + POSITION=2
+        self.assertEqual(M, 32+7+2, f"Count nodes = {M}")
+
+        
