7ca3ed84d1c281b9bec7161e5662433ce2187845
38dunn@gmail.com <Dann38>
2025-10-14 16:47:39 +0800

    fix_test_pdf_model

--- tests/test_pdf.py
+++ tests/test_pdf.py
@@ -6,12 +6,15 @@ from pager.metrics.levenshtein_distance import levenshtein_distance
 
 class TestPDF(unittest.TestCase):
     pdf_model=PDFModel()
-    pdf_model.read_from_file("files/text_header_table.pdf")    
-
-    with open("files/text_header_table_jar.json", 'r', encoding='utf-8') as f:
-        true_dict = json.load(f) 
+    with open("files/text_header_table_jar_words.json", 'r', encoding='utf-8') as f:
+        true_dict_words = json.load(f) 
+    
+    with open("files/text_header_table_jar_rows.json", 'r', encoding='utf-8') as f:
+        true_dict_rows = json.load(f) 
 
     def test_page_0_and_end(self) -> None:
+        self.pdf_model.clean_model()
+        self.pdf_model.read_from_file("files/text_header_table.pdf", method='w')
         page0 = self.pdf_model.to_dict()
         while not self.pdf_model.is_final_page():
             self.pdf_model.next_page()
@@ -19,9 +22,13 @@ class TestPDF(unittest.TestCase):
         self.assertEqual(page1, page0, f"{page0} \n != \n {page1}")
 
     def test_count_page(self) -> None:
-        self.assertEqual(self.pdf_model.count_page, len(self.true_dict['pages']))
+        self.pdf_model.clean_model()
+        self.pdf_model.read_from_file("files/text_header_table.pdf", method='w')
+        self.assertEqual(self.pdf_model.count_page, len(self.true_dict_words['pages']))
 
     def test_text(self) -> None:
+        self.pdf_model.clean_model()
+        self.pdf_model.read_from_file("files/text_header_table.pdf", method='w')
         page0 = self.pdf_model.to_dict()
         text = "" 
         with open('files/text_header_table_text.txt', 'r', encoding='utf-8') as f:
@@ -32,3 +39,20 @@ class TestPDF(unittest.TestCase):
             text_ += " " +  word['text']
         ld = levenshtein_distance(text, text_)/len(text)
         self.assertLessEqual(ld, 0.01)
+
+
+    def test_rows(self) -> None:
+        self.pdf_model.clean_model()
+        self.pdf_model.read_from_file("files/text_header_table.pdf", method='r')
+        page0 = self.pdf_model.to_dict()
+        text = "" 
+        with open('files/text_header_table_text.txt', 'r', encoding='utf-8') as f:
+            for line in f:
+                text += " " + line[:-1] # delete \n
+        text_ = ""
+        for row in page0['blocks']:  
+            text_ += " " +  row['text'][:-1] # delete \n
+        print(text_)
+        ld = levenshtein_distance(text, text_)/len(text)
+        self.assertLessEqual(ld, 0.01)
+        
