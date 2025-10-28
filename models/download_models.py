67cdbfaaa994249663748482a324863e25dac151
Dann38 <38dunn@gmail.com>
2025-10-28 08:19:53 +0800

    fit bert download test

--- models/download_models.py
+++ models/download_models.py
@@ -1,17 +1,23 @@
 import os
 from huggingface_hub import snapshot_download
+from transformers import BertTokenizer, BertModel
 
 def download_model():
+    model_name = "bert-base-multilingual-cased"
     current_file = os.path.realpath(__file__)
-    model_dir = os.path.join(os.path.dirname(current_file), 'bert')
-    snapshot_download(
-        repo_id="google-bert/bert-base-multilingual-cased",
-        cache_dir=model_dir,
-        allow_patterns=["*.json", "*.txt", "pytorch_model.bin", "tokenizer.json"],
-        etag_timeout=120,
-        force_download=True
-    )
-
+    cache_dir = os.path.join(os.path.dirname(current_file), 'bert')
+    model_dir = os.path.join(cache_dir, 'models--bert-base-multilingual-cased',
+                             'snapshots','3f076fdb1ab68d5b2880cb87a0886f315b8146f8')
+    print(model_dir)
+    try:
+        tokenizer = BertTokenizer.from_pretrained(model_dir)
+        model = BertModel.from_pretrained(model_dir).to('cpu')
+    except:
+        tokenizer = BertTokenizer.from_pretrained(model_name, cache_dir=cache_dir)
+        model = BertModel.from_pretrained(model_name, cache_dir=cache_dir).to('cpu')
+    inputs = tokenizer(['test', 'BERT'], return_tensors="pt", padding=True).to('cpu')
+    outputs = model(**inputs)
+    print(outputs)
 
 if __name__ == "__main__":
     download_model()
